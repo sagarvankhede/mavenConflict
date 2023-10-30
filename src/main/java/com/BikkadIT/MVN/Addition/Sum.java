5aaf494580c8d5faf222e289c291684dae8e32c0
sagarvankhede <svankhede48@gmail.com>
2023-10-30 14:04:36 +0530

    This is Additon Logic Project

---
+++ src/main/java/com/BikkadIT/MVN/Addition/Sum.java
@@ -0,0 +1,12 @@
+package com.BikkadIT.MVN.Addition;
+
+public class Sum {
+
+	
+	public int String(int A, int B) {
+		 int  C= A+B;
+		System.out.println("Addition Of Two Number-: " + C);
+		return C;
+		
+	}
+}
---
+++ src/main/java/com/BikkadIT/MVN/App.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.MVN;
+
+import com.BikkadIT.MVN.Addition.Sum;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Sum s =new Sum();
+        s.String(45, 85);
+        System.out.println(s);
+    }
+}
